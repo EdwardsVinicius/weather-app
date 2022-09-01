6bda7c7637b786960de9f5794cce3384ff80c232
Vinícius Edwards <vinicius.edwards@gmail.com>
2022-09-01 09:43:44 -0400

    Main activity and layout created

--- app/src/main/java/com/weather/MainActivity.kt
+++ app/src/main/java/com/weather/MainActivity.kt
@@ -2,10 +2,31 @@ package com.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.weather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private lateinit var cityName: TextView
+    private lateinit var temperature: TextView
+    private lateinit var weatherIcon: ImageView
+    private lateinit var weatherCondition: TextView
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        cityName = binding.activityMainCityName
+        temperature = binding.activityMainTemperature
+        weatherIcon = binding.activityMainWeatherIcon
+        weatherCondition = binding.activityMainWeatherCondition
+        recyclerView = binding.activityMainRecyclerView
+
     }
 }
